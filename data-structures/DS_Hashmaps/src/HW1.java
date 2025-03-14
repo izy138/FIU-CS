c1f7fb3fdd798d0e5dedfefd530ffe88cdf4d05b
izy138 <Isabellapc97@gmail.com>
2025-03-14 18:46:27 -0400

    Hashmaps assignment hw

--- data-structures/DS_Hashmaps/src/HW1.java
+++ data-structures/DS_Hashmaps/src/HW1.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 public class HW1 {
 
-    public static char findDistinguishingChat(String str1, String str2) {
+    public static char findDistinguishingChar(String str1, String str2) {
         Map<Character, Integer> freq1 = new HashMap<>();
         Map<Character, Integer> freq2 = new HashMap<>();
 
+        System.out.println("Frequency of first string: " + str1);
         // frequency for string 1
         for(char c : str1.toCharArray()) {
             freq1.put(c, freq1.getOrDefault(c, 0) + 1);
@@ -16,7 +17,7 @@ public class HW1 {
         for(char c : str2.toCharArray()) {
             freq2.put(c, freq2.getOrDefault(c, 0) + 1);
         }
-
+        System.out.println("\nFrequency of second string: " + str2);
         System.out.println(freq2);
         // hash set to hold unique characters in both strings
         // "hello" and "world" = [r, d, e, w, h, l, o]
@@ -24,6 +25,7 @@ public class HW1 {
         charSet.addAll(freq1.keySet());
         charSet.addAll(freq2.keySet());
 
+        System.out.println("\nSet of all characters:");
         System.out.println(charSet);
 
         char result = '\0';
@@ -39,6 +41,7 @@ public class HW1 {
                 result = c;
             }
         }
+        System.out.print("\nMost Distinguished Char: ");
         return result;
     }
 
@@ -59,6 +62,7 @@ public class HW1 {
                 oddCount++;
             }
         }
+        System.out.print(str + ": ");
         return oddCount <= 1;
     }
 
@@ -67,16 +71,20 @@ public class HW1 {
         for (int num : arr) {
             countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
-
+        System.out.println("Array: " + Arrays.toString(arr) );
+        System.out.print("Unique elements are: ");
         int sum = 0;
         // goes through each element in the HashMap
-        // checks if the value is 1 and adds it to sum
+        // checks if the frequency value is 1 and adds it to sum
         for (Map.Entry<Integer,Integer> entry : countMap.entrySet()) {
             if (entry.getValue() == 1) {
+                System.out.print(entry.getKey() + " ");
                 sum += entry.getKey();
             }
         }
+        System.out.print("\nSum of unique elements: ");
         return sum;
+
     }
 
     public static int countGreaterThanValue(BinaryNode<Integer> root, int value){
@@ -85,9 +93,11 @@ public class HW1 {
         }
         int count = 0;
 
+        // checks if the element is greater than the value given
         if(root.element > value) {
             count = 1;
         }
+        // recursively compares all elements of the tree with the value and add to the count
         count += countGreaterThanValue(root.left, value);
         count += countGreaterThanValue(root.right, value);
         return count;
@@ -147,29 +157,38 @@ public class HW1 {
     }
 
     public static void main(String[] args) {
-        System.out.println("\nProblem 1:");
-        System.out.println(findDistinguishingChat("abcde", "abbbbcccddee"));
+        System.out.println("\nProblem 1: ");
+        System.out.println("-----------------");
+        System.out.println(findDistinguishingChar("hello", "world"));
 
         System.out.println("\nProblem 2:");
+        System.out.println("-----------------");
+        System.out.println("Potential palindrome?");
         System.out.println(isAPotentialPalidrome("dads"));
         System.out.println(isAPotentialPalidrome("dad"));
         System.out.println(isAPotentialPalidrome("noon"));
 
         System.out.println("\nProblem 3:");
+        System.out.println("-----------------");
         System.out.println(sumOfUniqueElements(new int[]{1,2,2,3,4,4}));
+        System.out.println();
         System.out.println(sumOfUniqueElements(new int[]{1,2,3,4,4,4}));
 
         System.out.println("\nProblem 4:");
-        int element = 1;
+        System.out.println("-----------------");
+        int element = 3;
         BinaryNode<Integer> root4 = new BinaryNode<>(6);
         root4.left = new BinaryNode<>(3);
         root4.right = new BinaryNode<>(8);
         root4.left.left = new BinaryNode<>(1);
         root4.left.right = new BinaryNode<>(4);
+        root4.right.left = new BinaryNode<>(10);
+        root4.right.right = new BinaryNode<>(13);
+        System.out.println(root4.toString2());
         System.out.println("Elements greater than " + element + ": "+ countGreaterThanValue(root4, element));
 
         System.out.println("\nProblem 5:");
-
+        System.out.println("-----------------");
         BinaryNode<Integer> symmetricRoot = new BinaryNode<>(1);
         symmetricRoot.left = new BinaryNode<>(2);
         symmetricRoot.right = new BinaryNode<>(2);
@@ -177,8 +196,9 @@ public class HW1 {
         symmetricRoot.left.right = new BinaryNode<>(4);
         symmetricRoot.right.left = new BinaryNode<>(4);
         symmetricRoot.right.right = new BinaryNode<>(3);
-        System.out.println(symmetricRoot.toString2());
+        System.out.println("Tree is Symmetric?");
         System.out.println(isSymmetric(symmetricRoot));
+        System.out.println(symmetricRoot.toString2());
 
         BinaryNode<Integer> nonSymmetricRoot = new BinaryNode<>(1);
         nonSymmetricRoot.left = new BinaryNode<>(2);
@@ -186,10 +206,12 @@ public class HW1 {
         nonSymmetricRoot.left.left = new BinaryNode<>(3);
         nonSymmetricRoot.right.left = new BinaryNode<>(4);
         nonSymmetricRoot.right.right = new BinaryNode<>(3);
-        System.out.println(nonSymmetricRoot.toString2());
+        System.out.println("Tree is Symmetric?");
         System.out.println(isSymmetric(nonSymmetricRoot));
+        System.out.println(nonSymmetricRoot.toString2());
 
         System.out.println("\nProblem 6:");
+        System.out.println("-----------------");
         BinaryNode<Integer> problem6Tree = new BinaryNode<>(10);
         problem6Tree.left = new BinaryNode<>(8);
         problem6Tree.right = new BinaryNode<>(12);
@@ -197,13 +219,11 @@ public class HW1 {
         problem6Tree.left.right = new BinaryNode<>(9);
         problem6Tree.right.left = new BinaryNode<>(11);
         problem6Tree.right.right = new BinaryNode<>(13);
-        System.out.println("before removal:");
+        System.out.println("Before removal:");
         System.out.println(problem6Tree.toString2());
         removeLeaves(problem6Tree);
-        System.out.println("after removal:");
+        System.out.println("After removal:");
         System.out.println(problem6Tree.toString2());
-
-
 
     }
 
